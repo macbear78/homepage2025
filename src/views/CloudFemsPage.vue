633e8fbdaeceacdf710d3d7a800e3789683e9398
macbear78 <macbear78@gmail.com>
2025-10-19 18:21:10 +0900

    20251019_1820

--- src/views/CloudFemsPage.vue
+++ src/views/CloudFemsPage.vue
@@ -1,9 +1,20 @@
 <template>
-    <div>
-       <h2>클라우드FEMS</h2>
-       클라우드FEMS
+    <div class="relative w-screen h-screen overflow-hidden">
+    <!-- 전체화면 이미지 -->
+    <img
+      src="../assets/Desktop.png"
+      alt="클라우드FEMS 배경"
+      class="absolute top-0 left-0 w-full h-full object-cover"
+    />
+
+    <!-- 중앙 텍스트 -->
+    <div class="absolute inset-0 flex flex-col items-center justify-center text-white bg-black/40">
+      <h1 class="text-4xl font-bold mb-4">클라우드FEMS</h1>
+      <p class="text-lg">스마트 에너지 관리 시스템</p>
     </div>
+  </div>
 </template>
+
 
 <script setup>
 
