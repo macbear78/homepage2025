ebcb62ef16ae0314e93bb65e131aa281ae96c6b5
macbear78 <macbear78@gmail.com>
2025-10-20 13:38:07 +0900

    20251020_1337

--- src/views/OnlineQuotePage.vue
+++ src/views/OnlineQuotePage.vue
@@ -2,7 +2,18 @@
     <div>
        <h2>온라인견적</h2>
        온라인견적
+        <div class="flex flex-col space-y-3 text-center">
+            <router-link to="/OnlineQuote/EstimateList"  
+            class="font-semibold hover:text-teal-400 transition-colors duration-300">리스트</router-link>
+            <router-link to="/OnlineQuote/EstimateDetail"  
+            class="font-semibold hover:text-teal-400 transition-colors duration-300">읽기</router-link>     
+            <router-link to="/OnlineQuote/EstimateWrite"  
+            class="font-semibold hover:text-teal-400 transition-colors duration-300">쓰기</router-link>
+            <router-link to="/OnlineQuote/EstimateUpdate"  
+            class="font-semibold hover:text-teal-400 transition-colors duration-300">수정</router-link>  
+        </div>
     </div>
+    <router-view></router-view>
 </template>
 
 <script setup>
